1876122d90435d134eb762ab519256535ce84979
ashique357 <ashiquejubayer357@gmail.com>
2020-11-23 14:45:00 +0600

    .....

--- resources/views/Backend/Layouts/sidebar.blade.php
+++ resources/views/Backend/Layouts/sidebar.blade.php
@@ -1,133 +1,47 @@
-<div class="sidebar">
-      <!--
-        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red"
-    -->
-      <div class="sidebar-wrapper">
-        <div class="logo">
-          <a href="javascript:void(0)" class="simple-text logo-mini">
-            CT
-          </a>
-          <a href="javascript:void(0)" class="simple-text logo-normal">
-            Creative Tim
-          </a>
-        </div>
-        <ul class="nav">
-          <li class="active ">
-            <a href="./dashboard.html">
-              <i class="tim-icons icon-chart-pie-36"></i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li>
-            <a href="./icons.html">
-              <i class="tim-icons icon-atom"></i>
-              <p>Icons</p>
-            </a>
-          </li>
-          <li>
-            <a href="./map.html">
-              <i class="tim-icons icon-pin"></i>
-              <p>Maps</p>
-            </a>
-          </li>
-          <li>
-            <a href="./notifications.html">
-              <i class="tim-icons icon-bell-55"></i>
-              <p>Notifications</p>
-            </a>
-          </li>
-          <li>
-            <a href="./user.html">
-              <i class="tim-icons icon-single-02"></i>
-              <p>User Profile</p>
-            </a>
-          </li>
-          <li>
-            <a href="./tables.html">
-              <i class="tim-icons icon-puzzle-10"></i>
-              <p>Table List</p>
-            </a>
-          </li>
-          <li>
-            <a href="./typography.html">
-              <i class="tim-icons icon-align-center"></i>
-              <p>Typography</p>
-            </a>
-          </li>
-          <li>
-            <a href="./rtl.html">
-              <i class="tim-icons icon-world"></i>
-              <p>RTL Support</p>
-            </a>
-          </li>
-          <li class="active-pro">
-            <a href="./upgrade.html">
-              <i class="tim-icons icon-spaceship"></i>
-              <p>Upgrade to PRO</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-
-<!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-<!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
-  <div class="sidebar-brand-icon rotate-n-15">
-    <i class="fas fa-laugh-wink"></i>
-  </div>
-  <div class="sidebar-brand-text mx-3">DyeCamp</div>
-</a>
-
-<!-- Divider -->
-<hr class="sidebar-divider my-0">
-
-<!-- Nav Item - Dashboard -->
-<li class="nav-item active">
-  <a class="nav-link" href="">
-    <i class="fas fa-fw fa-tachometer-alt"></i>
-    <span>Dashboard</span></a>
-</li>
-
-  @foreach($menus as $key=>$menu)
-  <li class="nav-item">
-  @if($menu['childs'] == null && $menu['parent_id'] == 0)
-      <a class="nav-link" href="{{env('APP_URL')}}{{$menu['slug']}}">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>{{$menu['name']}}</span>
+<div class="sidebar" data="green">
+<div class="sidebar-wrapper">
+   <div class="logo">
+      <a href="" class="simple-text logo-mini">
+      dC
       </a>
-    @endif
+      <a href="" class="simple-text logo-normal">
+      DyeCamp
+      </a>
+   </div>
+   <ul class="nav">
+      <li class="active ">
+         <a href="">
+            <i class="tim-icons icon-chart-pie-36"></i>
+            <p>Dashboard</p>
+         </a>
+      </li>
 
-    @if($menu['childs'] != null)
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse{{$key}}" aria-expanded="true" aria-controls="collapseTwo{{$key}}">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>{{$menu['childs']['name']}}</span>
-        </a>
-        <div id="collapse{{$key}}" class="collapse" aria-labelledby="heading{{$key}}" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-              @if($menu['childs'] != null)
-                @foreach($menu['childs']['menu'] as $submenu)
-                <span>   
-                  <a  class="collapse-item" href="{{env('APP_URL')}}{{$submenu['slug']}}">{{$submenu['name'] }}</a>
-                </span>
-                @endforeach
-              @endif
-            </div>
-        </div>
-      
-    @endif
-</li>
-  @endforeach
-
-<!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
-
-<!-- Sidebar Toggler (Sidebar) -->
-<div class="text-center d-none d-md-inline">
-  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      @foreach($menus as $key=>$menu)
+      <li>
+         @if($menu['childs'] == null && $menu['parent_id'] == 0)
+         <a class="nav-link" href="{{env('APP_URL')}}{{$menu['slug']}}">
+            <i class="tim-icons icon-atom"></i>
+            <p>{{$menu['name']}}</p>
+         </a>
+         @endif
+         @if($menu['childs'] != null)
+         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapse{{$key}}" aria-expanded="true" aria-controls="collapseTwo{{$key}}">
+         <i class="fas fa-fw fa-cog"></i>
+         <span>{{$menu['childs']['name']}}</span>
+         </a>
+         <div id="collapse{{$key}}" class="collapse" aria-labelledby="heading{{$key}}" data-parent="#accordionSidebar">
+      <li class="bg-white py-2 collapse-inner rounded">
+      @if($menu['childs'] != null)
+      @foreach($menu['childs']['menu'] as $submenu)
+      <span>   
+      <a  class="collapse-item" href="{{env('APP_URL')}}{{$submenu['slug']}}">{{$submenu['name'] }}</a>
+      </span>
+      @endforeach
+      @endif
+      </li>    
+      </div>
+      @endif
+      </li>
+      @endforeach
+   </ul>
 </div>
-
-</ul>
-<!-- End of Sidebar -->
